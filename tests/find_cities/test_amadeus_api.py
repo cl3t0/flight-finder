8c98266983534c6c62adb081d65027ffd2e7efbb
Pedro Cleto <cl3t0.sh@gmail.com>
2023-01-28 15:40:39 -0300

    test: add integration test to amadeus api

---
+++ tests/find_cities/test_amadeus_api.py
@@ -0,0 +1,16 @@
+import pytest
+from find_cities.amadeus_api import AmadeusApi
+from decouple import config
+from datetime import date
+
+
+@pytest.mark.integ
+def test_check_price_between_gru_and_cnf():
+
+    key = config("API_KEY")
+    secret = config("API_SECRET")
+    url = config("API_URL")
+
+    client = AmadeusApi(key, secret, url)
+    price = client.get_price_between("GRU", "CNF", date(year=2023, month=4, day=1))
+    assert price > 0
