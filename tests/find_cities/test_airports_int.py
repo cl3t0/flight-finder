98ccddf9ca8831417da4ddf1ebf33d859b69ec8b
Pedro Cleto <cl3t0.sh@gmail.com>
2023-01-29 01:21:47 -0300

    test: add tests to new feature

--- tests/find_cities/test_airports_int.py
+++ tests/find_cities/test_airports_int.py
@@ -8,3 +8,13 @@ def test_get_all_airports():
     interface = AirportsInterface()
     all_airports = interface.get_all()
     assert len(all_airports) != 0
+
+
+@pytest.mark.integ
+def test_get_close_airports():
+
+    point = (14.127704, -84.562763)
+
+    interface = AirportsInterface()
+    airports = interface.get_close_airports(point, 5)
+    assert len(airports) != 0
